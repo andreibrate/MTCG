bf6db48497ffdbf6b8d977fc87330c5b20cf5438
Andrei Brate <andrei.brate@gmail.com>
2025-02-26 11:51:13 +0100

    Added Enums, Models, and packages.config

---
+++ MTCG/MTCG/Models/Card.cs
@@ -0,0 +1,27 @@
+﻿using MTCG.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTCG.Models
+{
+    public abstract class Card
+    {
+        public string Name { get; set; }
+        public double Damage { get; set; }
+        public Element ElementType { get; set; }
+        public CardType CardType { get; set; }
+
+
+        protected Card(string name, double damage, Element elementType, CardType cardType)
+        {
+            this.Name = name;
+            this.Damage = damage;
+            this.ElementType = elementType;
+            this.CardType = cardType;
+        }
+
+    }
+}
