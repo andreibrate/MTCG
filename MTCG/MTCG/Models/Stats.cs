2724cbfda85432d13f05cd8c00e10435e83ce811
Andrei Brate <andrei.brate@gmail.com>
2025-03-01 11:08:46 +0100

    Added user stats

---
+++ MTCG/MTCG/Models/Stats.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTCG.Models
+{
+    public class Stats
+    {
+        // constructor, start values
+        public Stats() {
+            Elo = 100;
+            Wins = 0;
+            Losses = 0;
+        }
+
+        public int Elo { get; set; } // Elo rating for leaderboard
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+    }
+}
