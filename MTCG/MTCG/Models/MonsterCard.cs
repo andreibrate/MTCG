4403ad004b95b0cde8846a07cbd5e5ac0dd39481
Andrei Brate <andrei.brate@gmail.com>
2025-03-01 09:49:56 +0100

    Added MonsterCard

---
+++ MTCG/MTCG/Models/MonsterCard.cs
@@ -0,0 +1,18 @@
+﻿using MTCG.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTCG.Models
+{
+    internal class MonsterCard : Card
+    {
+        public double Health { get; set; }
+        public MonsterCard(string name, double damage, double health, Element elementType, CardType cardType) : base(name, damage, elementType, cardType)
+        {
+            this.Health = health;
+        }
+    }
+}
