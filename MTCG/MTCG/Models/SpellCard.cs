5e385857a8ca2fa881ff710025337a97f85c442f
Andrei Brate <andrei.brate@gmail.com>
2025-03-01 09:53:11 +0100

    Added SpellCard

---
+++ MTCG/MTCG/Models/SpellCard.cs
@@ -0,0 +1,16 @@
+﻿using MTCG.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTCG.Models
+{
+    internal class SpellCard : Card
+    {
+        public SpellCard(string name, double damage, Element elementType, CardType cardType) : base(name, damage, elementType, cardType)
+        {
+        }
+    }
+}
