700cf3eac525459b8a42d4aa121c3ee405b3076b
Andrei Brate <andrei.brate@gmail.com>
2025-03-05 10:26:14 +0100

    Added Method enum (GET, POST, etc)

---
+++ MTCG/Http/HttpMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTCG.Http
+{
+    public enum HttpMethod
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE,
+    }
+}
