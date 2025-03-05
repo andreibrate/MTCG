fce95f966a320b80a036d43d0145f7d3d68806b0
Andrei Brate <andrei.brate@gmail.com>
2025-03-05 10:49:17 +0100

    Added Enpoints folder and IHttpEndpoint interface

---
+++ MTCG/Http/Endpoints/IHttpEndpoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTCG.Http.Endpoints
+{
+    internal interface IHttpEndpoint
+    {
+        bool HandleRequest(HttpRequest request, HttpResponse response);
+    }
+}
