b54dc440f987ae35ab9f52f29a925a313a06ebbd
Andrei Brate <andrei.brate@gmail.com>
2025-03-05 18:55:35 +0100

    Added HttpServer

---
+++ MTCG/Http/HttpServer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using MTCG.Http.Endpoints;
+
+namespace MTCG.Http
+{
+    internal class HttpServer
+    {
+        private readonly CancellationTokenSource cts = new CancellationTokenSource();
+
+        private readonly int port = 10001;
+        private readonly IPAddress ip = IPAddress.Loopback;
+
+        private TcpListener tcpListener;
+        public Dictionary<string, IHttpEndpoint> Endpoints { get; private set; } = new Dictionary<string, IHttpEndpoint>();
+
+        public HttpServer()
+        {
+            port = 10001;
+            ip = IPAddress.Loopback;
+
+            tcpListener = new TcpListener(ip, port);
+        }
+
+        public HttpServer(int port)
+        {
+            this.port = port;
+            ip = IPAddress.Loopback;
+
+            tcpListener = new TcpListener(ip, port);
+        }
+        public HttpServer(IPAddress ip, int port)
+        {
+            this.port = port;
+            this.ip = ip;
+
+            tcpListener = new TcpListener(ip, port);
+        }
+
+    }
+}
