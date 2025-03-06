37a94e68bed12c9b441cea9b31ccc9632842b195
Andrei Brate <andrei.brate@gmail.com>
2025-03-06 18:59:54 +0100

    Changed Card

--- MTCG/Models/Card.cs
+++ MTCG/Models/Card.cs
@@ -12,18 +12,18 @@ namespace MTCG.Models
         public Guid Id { get; set; }
         public string Name { get; set; }
         public double Damage { get; set; }
-        public Element ElementType { get; set; }
+        public Element Element { get; set; }
         public CardType CardType { get; set; }
         public Guid OwnerId { get; set; }
         public bool isLocked { get; set; }
 
 
-        protected Card(string name, double damage, Element elementType, CardType cardType, Guid ownerId)
+        protected Card(string name, double damage, Element element, CardType cardType, Guid ownerId)
         {
             this.Id = Guid.NewGuid();
             this.Name = name;
             this.Damage = damage;
-            this.ElementType = elementType;
+            this.Element = element;
             this.CardType = cardType;
             this.OwnerId = ownerId;
             this.isLocked = false;
