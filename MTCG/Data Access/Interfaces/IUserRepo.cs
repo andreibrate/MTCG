a08b3b6ab6dfb56696e05497b8e3a2ad0940690e
Andrei Brate <andrei.brate@gmail.com>
2025-03-06 18:41:00 +0100

    Fixed IUserRepo (made public)

--- MTCG/Data Access/Interfaces/IUserRepo.cs
+++ MTCG/Data Access/Interfaces/IUserRepo.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MTCG.Models;
 
 namespace MTCG.Data_Access.Interfaces
 {
-    internal interface IUserRepo
+    public interface IUserRepo
     {
+        bool RegisterUser(string username, string password);
+        User? LoginUser(string username, string password);
+        User? GetUserToken(string token);
+        void UpdateUser(User user);
+        List<User> GetUsers();
+        List<User> GetUsersStartingWith(string prefix);
+        IEnumerable<(string Username, Stats Stats)> GetScoreboardStats();
     }
 }
