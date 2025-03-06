7367a0c2762e7aea9bd6abc0ab503a53ad82ea6b
Andrei Brate <andrei.brate@gmail.com>
2025-03-06 19:52:09 +0100

    Added CardRepo (now complete)

--- MTCG/Data Access/CardRepo.cs
+++ MTCG/Data Access/CardRepo.cs
@@ -72,22 +72,93 @@ namespace MTCG.Data_Access
             }
         }
 
-
-
-
-
-
         public Card? GetCardById(Guid cardId)
         {
-            throw new NotImplementedException();
+            using var connection = new NpgsqlConnection(_connectionString);
+            connection.Open();
+
+            using var command = connection.CreateCommand();
+            command.CommandText = @"
+                SELECT Id, Name, Damage, Element, Tribe, CardType, OwnerId, IsLocked
+                FROM Cards
+                WHERE Id = @CardId;
+            ";
+            command.Parameters.AddWithValue("@CardId", cardId);
+
+            using var reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                var id = reader.GetGuid(0);
+                var name = reader.GetString(1);
+                var damage = reader.GetDouble(2);
+                var element = (Element)reader.GetInt32(3);
+                var cardType = (CardType)reader.GetInt32(5);
+                var ownerId = reader.GetGuid(6);
+                var isLocked = reader.GetBoolean(7);
+
+                if (cardType == CardType.Monster)
+                {
+                    var tribe = (Tribe)reader.GetInt32(4);
+                    return new MonsterCard(name, damage, element, tribe, ownerId) { IsLocked = isLocked, Id = id };
+                }
+                else
+                {
+                    return new SpellCard(name, damage, element, ownerId) { IsLocked = isLocked, Id = id };
+                }
+            }
+            return null;
         }
+
         public bool AddCard(Card card)
         {
-            throw new NotImplementedException();
+            using var connection = new NpgsqlConnection(_connectionString);
+            connection.Open();
+
+            using var command = connection.CreateCommand();
+            if (card is MonsterCard monsterCard)
+            {
+                command.CommandText = @"
+                    INSERT INTO Cards (Id, Name, Damage, Element, Tribe, CardType, OwnerId, IsLocked)
+                    VALUES (@Id, @Name, @Damage, @Element, @Tribe, @CardType, @OwnerId, @IsLocked)
+                ";
+                command.Parameters.AddWithValue("@Tribe", (int)monsterCard.Tribe);
+            }
+            else
+            {
+                command.CommandText = @"
+                    INSERT INTO Cards (Id, Name, Damage, Element, CardType, OwnerId, IsLocked)
+                    VALUES (@Id, @Name, @Damage, @Element, @CardType, @OwnerId, @IsLocked)
+                ";
+            }
+
+            command.Parameters.AddWithValue("@Id", card.Id);
+            command.Parameters.AddWithValue("@Name", card.Name);
+            command.Parameters.AddWithValue("@Damage", card.Damage);
+            command.Parameters.AddWithValue("@Element", (int)card.Element);
+            command.Parameters.AddWithValue("@CardType", (int)card.CardType);
+            command.Parameters.AddWithValue("@OwnerId", card.OwnerId);
+            command.Parameters.AddWithValue("@IsLocked", card.IsLocked);
+
+            return command.ExecuteNonQuery() > 0;
         }
+
         public void UpdateCard(Card card)
         {
-            throw new NotImplementedException();
+            using var connection = new NpgsqlConnection(_connectionString);
+            connection.Open();
+
+            using var command = connection.CreateCommand();
+            command.CommandText = @"
+                UPDATE Cards
+                SET OwnerId = @OwnerId,
+                    IsLocked = @IsLocked
+                WHERE Id = @Id
+            ";
+            command.Parameters.AddWithValue("@OwnerId", card.OwnerId);
+            command.Parameters.AddWithValue("@IsLocked", card.IsLocked);
+            command.Parameters.AddWithValue("@Id", card.Id);
+
+            command.ExecuteNonQuery();
         }
     }
 }
