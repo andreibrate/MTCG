875ee9dd66c8f397eb9ea7711c125b8230a21574
Andrei Brate <andrei.brate@gmail.com>
2025-03-07 19:19:47 +0100

    Added Newtonsoft.Json NuGet package (forgot to commit before JsonConverter)

--- MTCG/obj/Debug/net8.0/MTCG.AssemblyInfo.cs
+++ MTCG/obj/Debug/net8.0/MTCG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTCG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9453e0e21f15f53a30db65b5aa11c24b7f698471")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56aac9c9354209dcfa1e059a2108792ad7fb8393")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTCG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTCG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
