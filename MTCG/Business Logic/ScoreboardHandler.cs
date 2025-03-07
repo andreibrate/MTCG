6a315a47fe2ea4ad986aaed645a8b53f6ecf71e3
Andrei Brate <andrei.brate@gmail.com>
2025-03-07 21:22:53 +0100

    Added ScoreboardHandler

---
+++ MTCG/Business Logic/ScoreboardHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MTCG.Data_Access.Interfaces;
+using MTCG.Models;
+
+namespace MTCG.Business_Logic
+{
+    public class ScoreboardHandler
+    {
+        private readonly IUserRepo _userRepo;
+
+        public ScoreboardHandler(IUserRepo userRepo)
+        {
+            _userRepo = userRepo;
+        }
+
+        public IEnumerable<(string Username, Stats Stats)> GetScoreboard()
+        {
+            return _userRepo.GetScoreboardStats();
+        }
+    }
+}
--- MTCG/obj/Debug/net8.0/MTCG.AssemblyInfo.cs
+++ MTCG/obj/Debug/net8.0/MTCG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTCG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96adec7f95912e7eb99944e7ceac05acec5c43c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+967a0a2cf696ef1e9d42eabe5d37db532e1f8261")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTCG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTCG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
