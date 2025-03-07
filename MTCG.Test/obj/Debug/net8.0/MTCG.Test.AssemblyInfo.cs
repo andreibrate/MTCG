9c3cdc82b8874d48de3c85364de161eecfc214d2
Andrei Brate <andrei.brate@gmail.com>
2025-03-07 23:41:28 +0100

    Added rest of functionality to main program

--- MTCG.Test/obj/Debug/net8.0/MTCG.Test.AssemblyInfo.cs
+++ MTCG.Test/obj/Debug/net8.0/MTCG.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTCG.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84a34da027ef4bb35727854371f6261a6ec5bf48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d971f976cade2ceed66beede3b36df28a23d1654")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTCG.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTCG.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MTCG/Program.cs
+++ MTCG/Program.cs
@@ -1,11 +1,12 @@
 ﻿using MTCG.Http.Endpoints;
 using MTCG.Http;
-// Data Access Layer
-// Business Logic Layer
+using MTCG.Data_Access;
+using MTCG.Data_Access.Interfaces;
+using MTCG.Business_Logic;
+using Npgsql;
 using System;
 using System.Net;
 using System.Threading;
-// using Npgsql;
 
 namespace MTCG
 {
@@ -17,10 +18,95 @@ namespace MTCG
 
             // initialize connection string for DB connection
             string connectionString = "Host=localhost;Username=postgres;Password=postgres;Database=mtcgdb";
-            // Initialize the Http Server
+            // initialize http server
             HttpServer? server = null;
 
+            try
+            {
+                Console.WriteLine("=*=*=*=[ Monster Trading Card Game ]=*=*=*=");
 
+                // Initialize DB / connection
+                //#if DEBUG
+                Console.WriteLine("Cleaning up Database...");
+                DbManager.CleanupTables(connectionString);
+                //#endif
+                Console.WriteLine("Initializing Database...");
+                DbManager.InitializeDatabase(connectionString);
+
+                // initialize BattleQueue
+                BattleQueue battleQueue = new BattleQueue();
+
+                // create repos
+                IUserRepo userRepo = new UserRepo(connectionString);
+                IPackRepo packRepo = new PackRepo(connectionString);
+                ICardRepo cardRepo = new CardRepo(connectionString);
+                IDeckRepo deckRepo = new DeckRepo(connectionString);
+                ITradeRepo tradeRepo = new TradeRepo(connectionString);
+
+                // initialize handlers
+                UserHandler userHandler = new UserHandler(userRepo);
+                PackHandler packHandler = new PackHandler(packRepo);
+                TransactionHandler transactionHandler = new TransactionHandler(userHandler, packRepo);
+                CardHandler cardHandler = new CardHandler(cardRepo);
+                DeckHandler deckHandler = new DeckHandler(deckRepo, cardRepo);
+                ScoreboardHandler scoreboardHandler = new ScoreboardHandler(userRepo);
+                TradeHandler tradeHandler = new TradeHandler(tradeRepo, cardRepo, userRepo);
+                BattleHandler battleHandler = new BattleHandler(userRepo, deckRepo, cardRepo);
+
+                // setup http server
+                Console.WriteLine("Setting up server...");
+                server = new HttpServer();
+                var serverThread = new Thread(() => server.Run());
+
+                // register endpoints for http server
+
+                //TODO:
+                //server.RegisterEndpoint("users", new UsersEndpoint(userHandler));                       // Registers Endpoint for user registration
+                //server.RegisterEndpoint("sessions", new SessionsEndpoint(userHandler));                 // Registers Endpoint for user login
+                //server.RegisterEndpoint("packages", new PacksEndpoint(packHandler));              // Registers Endpoint for card packs (creating packs)
+                //server.RegisterEndpoint("transactions", new TransactionsEndpoint(transactionHandler));  // Registers Endpoint for transactions regarding card packs
+                //server.RegisterEndpoint("cards", new CardsEndpoint(userHandler, cardHandler));          // Registers Endpoint for card listing
+                //server.RegisterEndpoint("deck", new DeckEndpoint(userHandler, deckHandler));            // Registers Endpoint for managing the user deck
+                //server.RegisterEndpoint("stats", new StatsEndpoint(userHandler));                       // Registers Endpoint for viewing user statistics
+                //server.RegisterEndpoint("scoreboard", new ScoreboardEndpoint(scoreboardHandler));       // Registers Endpoint for viewing the scoreboard
+                //server.RegisterEndpoint("trades", new TradesEndpoint(tradeHandler));              // Registers Endpoint for trading cards
+                //server.RegisterEndpoint("battles", new BattleEndpoint(battleHandler, battleQueue));     // Registers Endpoint for battles between players
+
+                // start Server
+                serverThread.Start();
+
+                // open shut down option for server
+                Console.WriteLine("Press 'q' to stop the server...");
+                while (Console.ReadKey(true).Key != ConsoleKey.Q)
+                {
+                    Thread.Sleep(100); // to avoid unnecessary resource usage
+                }
+                server.Stop();
+                serverThread.Join();
+            }
+            catch (NpgsqlException ex)
+            {
+                // handle PostgreSQL exceptions
+                Console.WriteLine("A PostgreSQL exception occurred: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                // generic exception handling for other unexpected errors
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+            finally
+            {
+                // cleanup
+                Console.WriteLine("Stopping server...");
+                server?.Stop();
+
+                //#if DEBUG
+                Console.WriteLine("Cleaning up Database...");
+                DbManager.CleanupTables(connectionString);
+                //#endif
+
+                Console.WriteLine("... Program End");
+            }
         }
     }
 }
