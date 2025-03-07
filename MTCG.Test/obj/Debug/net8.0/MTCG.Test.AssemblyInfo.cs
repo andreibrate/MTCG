668c2909596aa129979cdb6781326672934f0cfd
Andrei Brate <andrei.brate@gmail.com>
2025-03-07 22:31:32 +0100

    Added folders (BL, DA, EP) in test project

--- MTCG.Test/obj/Debug/net8.0/MTCG.Test.AssemblyInfo.cs
+++ MTCG.Test/obj/Debug/net8.0/MTCG.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTCG.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b2de1b2cc80cae69f72612a358a9cab090699cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84a34da027ef4bb35727854371f6261a6ec5bf48")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTCG.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTCG.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
