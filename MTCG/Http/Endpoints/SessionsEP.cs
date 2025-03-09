c7e9addf0cd1d555ca82955e38187ea0385d4ba9
Andrei Brate <andrei.brate@gmail.com>
2025-03-09 15:35:30 +0100

    Added SessionsEP

---
+++ MTCG/Http/Endpoints/SessionsEP.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using MTCG.Business_Logic;
+using MTCG.Models;
+
+namespace MTCG.Http.Endpoints
+{
+    internal class SessionsEP : IHttpEndpoint
+    {
+        private readonly UserHandler _userHandler;
+
+        public SessionsEP(UserHandler userHandler)
+        {
+            _userHandler = userHandler;
+        }
+
+        public bool HandleRequest(HttpRequest rq, HttpResponse rs)
+        {
+            if (rq.Method == HttpMethod.POST && rq.Path[1] == "sessions")
+            {
+                if (string.IsNullOrEmpty(rq.Content))
+                {
+                    rs.SetClientError("No content provided", 400);
+                    return true;
+                }
+
+                // Deserialize for login
+                var userData = JsonSerializer.Deserialize<User>(rq.Content);
+                if (userData == null || string.IsNullOrEmpty(userData.Username) || string.IsNullOrEmpty(userData.Password))
+                {
+                    rs.SetClientError("Invalid login data provided", 400);
+                    return true;
+                }
+
+                // Call the UserHandler to log the user in
+                string? token = _userHandler.LoginUser(userData.Username, userData.Password);
+                if (token != null)
+                {
+                    rs.SetSuccess("Login successful", 200);
+                    rs.Content = JsonSerializer.Serialize(new { Token = token });
+                }
+                else
+                {
+                    rs.SetClientError("Login failed", 401); // Unauthorized
+                }
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- MTCG/obj/Debug/net8.0/MTCG.AssemblyInfo.cs
+++ MTCG/obj/Debug/net8.0/MTCG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTCG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc9153eef7bbb9139b3400c099d328eae1fbb58e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f2af899bef95760d341c4d4532758c84a824d9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTCG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTCG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
